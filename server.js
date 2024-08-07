c361f4628fd116f84122135f786db2ea686d4045
Harsh Raj <118976504+returnHarsh@users.noreply.github.com>
2024-08-07 22:56:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors({
-    origin : ["http://localhost:3000" , "http://localhost:3001"],
+    origin : ["http://localhost:3000" , "http://localhost:3001" , "*" ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials : true
 }))
